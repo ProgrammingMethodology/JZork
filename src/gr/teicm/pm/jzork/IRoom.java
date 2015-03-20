11af61756730a69fd197d36891148cb201981d92
MariaPol <mariatsimi@hotmail.com>
2015-03-20 15:34:23 +0200

    IRoom class

---
+++ src/gr/teicm/pm/jzork/IRoom.java
@@ -0,0 +1,13 @@
+package gr.teicm.pm.jzork;
+
+/**
+ *
+ * @author Maria
+ */
+public interface IRoom {
+    
+    public void setExit(Room north, Room east, Room south, Room west);
+    public Room getExit(String direction);
+    public String shortDescription();
+    public String getLongDescription();
+}
